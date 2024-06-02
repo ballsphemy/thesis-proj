228870014b157f617b2ead6cd75a6f2705478401
ballsphemy <ballsphemy@gmail.com>
2024-06-02 21:30:01 +0800

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,63 +1,66 @@
+// Import dependencies
 import path from "path";
 import express from "express";
-import dotevn from "dotenv";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-dotevn.config();
-import products from "./data/products.js";
+dotenv.config();
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoute.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
+
 const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
 
-// body parser middleware
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
+// Cookie parser middleware
 app.use(cookieParser());
 
-// app.get('/api/products', (req, res) => {
-//   res.json(products);
-// })
-
-// app.get('/api/products/:id', (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id);
-//   res.json(product);
-// })
-
+// API Routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
+// PayPal Configuration Route
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
+// Serve Static Files
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  // set static folder
+  console.log("Serving static files from frontend/build");
   app.use(express.static(path.join(__dirname, "/frontend/build")));
-  // any route that is not api will be redirected to index.html
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    console.log(
+      `Serving file: ${path.resolve(
+        __dirname,
+        "frontend",
+        "build",
+        "index.html"
+      )}`
+    );
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running");
   });
 }
+
+// Error Handling Middleware
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+// Start Server
+app.listen(port, () => console.log(`Server running on port ${port}`));
