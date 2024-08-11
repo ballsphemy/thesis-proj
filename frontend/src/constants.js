7832aa8fc63562021e5a63db8c48302d334f1941
ballsphemy <ballsphemy@gmail.com>
2024-08-11 16:01:57 +0800

    test constant

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,6 +2,9 @@ export const BASE_URL =
   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
-export const ORDERS_URL = "/api/orders";
+export const ORDERS_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://vmapaytrading.us.to/'
+    : '/api/orders';
 export const PAYPAL_URL = "/api/config/paypal";
 export const UPLOADS_URL = "/api/upload";
